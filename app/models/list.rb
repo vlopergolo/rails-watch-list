5d65eb19759d325bc8fd8d64f6d475653b8eac4a
Vincenzo Lopergolo <vincenzolopergolo@gmail.com>
2021-04-20 18:48:24 +0200

    solved

---
+++ app/models/list.rb
@@ -0,0 +1,8 @@
+class List < ApplicationRecord
+    has_many :bookmarks, dependent: :destroy
+    has_many :movies, through: :bookmarks
+    has_many :reviews
+  
+    validates :name, uniqueness: true, presence: true
+  end
+  
